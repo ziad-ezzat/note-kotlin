e5adc6849300b1299c9eab124130dc003b6bd6fa
ziad-ezzat <ezzatziad83@gmail.com>
2023-06-03 21:45:31 +0200

    all work

--- app/src/main/java/com/example/android/roomwordssample/Retrofit/NotesApiService.kt
+++ app/src/main/java/com/example/android/roomwordssample/Retrofit/NotesApiService.kt
@@ -1,12 +1,11 @@
 package com.example.android.roomwordssample.Retrofit
 
 import com.example.android.roomwordssample.model.Note
-import okhttp3.ResponseBody
-import retrofit2.Response
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface NoteService {
-    @POST("notes")
-    suspend fun postNote(@Body note: Note): Response<Note>
+    @POST("notes.json")
+    fun postNote(@Body note: Note): Call<Note>
 }
--- app/src/main/java/com/example/android/roomwordssample/WorkManager/PostNotesWorker.kt
+++ app/src/main/java/com/example/android/roomwordssample/WorkManager/PostNotesWorker.kt
@@ -9,35 +9,75 @@ import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.android.roomwordssample.R
 import com.example.android.roomwordssample.Repository.NoteRepository
-import com.example.android.roomwordssample.Retrofit.RetrofitClient
+import com.example.android.roomwordssample.Retrofit.NoteService
+import com.example.android.roomwordssample.model.Note
 import com.example.android.roomwordssample.model.NoteRoomDatabase
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class PostNotesWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
 
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://test-13589-default-rtdb.firebaseio.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+    var apiInterface: NoteService = retrofit.create(NoteService::class.java)
+
     override fun doWork(): Result {
-        val noteRepository = NoteRepository(NoteRoomDatabase.getDatabase(applicationContext, CoroutineScope(
-            Dispatchers.IO)).noteDao())
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val notes = noteRepository.allWords.firstOrNull()
+        val noteRepository = NoteRepository(
+            NoteRoomDatabase.getDatabase(
+                applicationContext,
+                CoroutineScope(Dispatchers.IO)
+            ).noteDao()
+        )
 
-            try {
-                val noteService = RetrofitClient.noteService
-                for (note in notes!!) {
-                    val response = noteService.postNote(note)
+        val notes = runBlocking { noteRepository.allWords.firstOrNull() }
+
+        try {
+            if (notes != null) {
+                for (note in notes) {
+                    val call = apiInterface.postNote(note)
+                    call.enqueue(object : Callback<Note> {
+                        override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                            if (response.isSuccessful) {
+                                showNotification("Post Completed 1", "The note was posted successfully")
+                            } else {
+                                showNotification("Post Failed 2", "Failed to post the note")
+                            }
+                        }
+                        override fun onFailure(call: Call<Note>, t: Throwable) {
+                            showNotification("Error 3", "Failed to make the network request")
+                        }
+                    })
+                }
+            }
+        } catch (e: Exception) {
+            showNotification("Error 4", "Failed to fetch notes")
+            val call = apiInterface.postNote(Note("test"))
+            call.enqueue(object : Callback<Note> {
+                override fun onResponse(call: Call<Note>, response: Response<Note>) {
                     if (response.isSuccessful) {
-                        showNotification("Post Completed","The note was posted successfully")
+                        showNotification("Post Completed 5", "The note was posted successfully")
                     } else {
-                        showNotification("Post Failed","Failed to post the note")
+                        showNotification("Post Failed 6", "Failed to post the note")
                     }
                 }
-            } catch (e: Exception) {
-                showNotification("Post Failed","Failed to post the note")
-            }
+                override fun onFailure(call: Call<Note>, t: Throwable) {
+                    showNotification("Error 7", "Failed to make the network request")
+                }
+            })
         }
 
         return Result.success()
